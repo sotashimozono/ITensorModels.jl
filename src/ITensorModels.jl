3da57f7ea3702e4873c7aa0925336f0aa9c9f58c
sotashimozono <shimozono-sota631@g.ecc.u-tokyo.ac.jp>
2026-05-12 13:00:56 +0000

    feat(models): add PXP1D (Rydberg blockade, many-body scars)
    bump version to 0.7.10

--- src/ITensorModels.jl
+++ src/ITensorModels.jl
@@ -20,6 +20,7 @@ export J1J2Heisenberg1D
 export BoseHubbard1D
 export DMIHeisenberg1D
 export LongRangeXY1D
+export PXP1D
 export TFIM, TFIML, XXZ1D, Heisenberg1D, S1Heisenberg1D, KitaevBond, LatticeModel
 export TFIM, TFIML, XXZ1D, Heisenberg1D, KitaevBond, TightBinding1D, LatticeModel
 export AbstractModulation, Uniform, SSD, SinPower, SmoothBoundary, Tabulated
@@ -87,6 +88,7 @@ include("models/j1j2_heisenberg_1d.jl")
 include("models/bose_hubbard_1d.jl")
 include("models/dmi_heisenberg_1d.jl")
 include("models/long_range_xy_1d.jl")
+include("models/pxp_1d.jl")
 include("models/kitaev_bond.jl")
 include("models/xy_h_1d.jl")
 include("models/long_range_ising_1d.jl")
